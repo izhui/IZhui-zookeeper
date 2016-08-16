bead43f1ea8263392e946633b3681278a30e995b
I'm EE <admin@cavacn.com>
2016-08-16 15:55:49 +0800

    可运行v1版本

--- example/mytest.js
+++ example/mytest.js
@@ -4,5 +4,11 @@
 'use strict';
 const MyZookeeper = require("../lib");
 
-let zk = new MyZookeeper("我的测试案例","/mydemo");
+const zk = new MyZookeeper("我的测试案例","/mydemo");
 
+zk.on("data",function(){
+    // console.log(zk.root.$.mydemo)
+})
+setInterval(function(){
+    console.log(zk.root.$.mydemo)
+},1000)
